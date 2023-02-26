adba072ed3bf4eedcafa921ebed0717544d555b1
Archilg456 <gabaidze555@gmail.com>
2023-02-26 23:10:19 +0400

    hii

--- src/components/registration/RegisterForm.js
+++ src/components/registration/RegisterForm.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { useForm } from "../../applications";
-import { generateRegisterFormValue } from "./generateRegisterFormValue";
 
 export const RegisterForm = () => {
-  let {} = useForm({ defaultFormValue: generateRegisterFormValue() });
   return <div>RegisterForm</div>;
 };
+  
