c0d86d12b86c12ae3436a71e5192d3e1be3158a1
Archilg456 <gabaidze555@gmail.com>
2023-03-22 20:44:51 +0400

    add search input (need fiix no writing consol)

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -1,5 +1,51 @@
-import React from "react";
+import { Autocomplete, Box, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const results = [];
+  const [searchQuery, setSearchQuery] = useState("");
+  console.log("searchQuery:", searchQuery);
+
+  return (
+    <Autocomplete
+      freeSolo
+      sx={{
+        width: 700,
+        background: "#fff",
+        borderRadius: 5,
+        outline: "none",
+        margin: 2,
+      }}
+      disableClearable
+      options={results}
+      getOptionLabel={(option) => option.name}
+      renderOption={(_, option) => {
+        const { name, _id, category, price } = option;
+        return (
+          <Link
+            to={`/products/categories/${category}/${name}`}
+            key={_id}
+            state={{ id: _id }}
+          >
+            <Box>
+              <Typography>{name}</Typography>
+              <Typography>{price}</Typography>
+            </Box>
+          </Link>
+        );
+      }}
+      renderInput={(params) => {
+        return (
+          <TextField
+            {...params}
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            label="Search"
+            InputProps={{ ...params.inputProps, type: "search" }}
+          />
+        );
+      }}
+    />
+  );
 };
